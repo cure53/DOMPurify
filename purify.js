ae80b10e5258c299bce728297da5ef588022728a
Frederic Hemberger <fhemberger@users.noreply.github.com>
2014-10-07 17:54:00 +0200

    Early exit of parsing if string doesn't seem to contain HTML

--- purify.js
+++ purify.js
@@ -170,6 +170,8 @@
          * @return a DOM, filled with the dirty markup
          */
         var _initDocument = function(dirty){
+            /* Exit directly if we have nothing to do */
+            if (dirty.indexOf('<') === -1) { return dirty; }
             
             /* Create documents to map markup to */
             var dom = document.implementation.createHTMLDocument('');
