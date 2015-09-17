cc2a8731719c53538b3bc93c00081add39d9513a
Mario Heiderich <mario@cure53.de>
2015-09-17 13:13:50 +0200

    Removed some unecessary code Removed checks against document.implementation Added Safari 8.0 browser config for CI

--- src/purify.js
+++ src/purify.js
@@ -38,6 +38,7 @@
     var NamedNodeMap = window.NamedNodeMap || window.MozNamedAttrMap;
     var Text = window.Text;
     var Comment = window.Comment;
+    var DOMParser = window.DOMParser;
 
     // As per issue #47, the web-components registry is inherited by a
     // new document created via createHTMLDocument. As per the spec
@@ -48,7 +49,6 @@
     if (typeof HTMLTemplateElement === 'function') {
         document = document.createElement('template').content.ownerDocument;
     }
-    var implementation = document.implementation;
     var createNodeIterator = document.createNodeIterator;
     var getElementsByTagName = document.getElementsByTagName;
     var createDocumentFragment = document.createDocumentFragment;
@@ -60,8 +60,7 @@
      * Expose whether this browser supports running the full DOMPurify.
      */
     DOMPurify.isSupported =
-        typeof implementation.createHTMLDocument !== 'undefined' &&
-        document.documentMode !== 9;
+        typeof DOMParser !== 'undefined' && document.documentMode !== 9;
 
     /* Add properties to a lookup table */
     var _addToSet = function(set, array) {
@@ -303,9 +302,7 @@
      */
     var _initDocument = function(dirty) {
 
-        var parser = new DOMParser();
-        var doc = parser.parseFromString(dirty, "text/html");
-        var body = doc.body;
+        var doc = new DOMParser().parseFromString(dirty, "text/html");
 
         /* Work on whole document or just its body */
         return getElementsByTagName.call(doc,
