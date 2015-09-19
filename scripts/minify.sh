180f17db6fa4e9c67669b06bc28533971ba1a8b7
suǝʞǝǝpʇ <tobias.deekens@epost-dev.de>
2015-09-19 20:58:06 +0200

    Refactor minification result to be amended after test passed.

--- scripts/minify.sh
+++ scripts/minify.sh
@@ -6,7 +6,3 @@ echo "# Minifying purify.js using Uglifyjs2..."
 
 ./node_modules/.bin/uglifyjs ./src/purify.js -o ./dist/purify.min.js \
   --mangle --comments --source-map ./dist/purify.min.js.map
-
-echo "# Amending minified assets to HEAD"
-
-git add ./dist/purify.min.js ./dist/purify.min.js.map && git commit --amend --no-verify -C HEAD
