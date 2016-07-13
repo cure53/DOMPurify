2cc594b0dc00ec3472d8d24dde7b4ce2bab4d93d
adrien <adrien@buildog.com>
2016-07-13 14:32:38 +0200

    Add new schemes in allowed protocol list

--- src/purify.js
+++ src/purify.js
@@ -570,7 +570,7 @@ function createDOMPurify(window = getGlobal()) {
 
   const DATA_ATTR = /^data-[\-\w.\u00B7-\uFFFF]/; // eslint-disable-line no-useless-escape
   const ARIA_ATTR = /^aria-[\-\w]+$/; // eslint-disable-line no-useless-escape
-  const IS_ALLOWED_URI = /^(?:(?:(?:f|ht)tps?|mailto|tel):|[^a-z]|[a-z+.\-]+(?:[^a-z+.\-:]|$))/i; // eslint-disable-line no-useless-escape
+  const IS_ALLOWED_URI = /^(?:(?:(?:f|ht)tps?|mailto|tel|callto|cid|xmpp):|[^a-z]|[a-z+.\-]+(?:[^a-z+.\-:]|$))/i; // eslint-disable-line no-useless-escape
   const IS_SCRIPT_OR_DATA = /^(?:\w+script|data):/i;
   /* This needs to be extensive thanks to Webkit/Blink's behavior */
   const ATTR_WHITESPACE = /[\u0000-\u0020\u00A0\u1680\u180E\u2000-\u2029\u205f\u3000]/g;
