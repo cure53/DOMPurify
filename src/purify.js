aeff130200ccb13de255847e62a9c01d862c351f
Mario Heiderich <mario@cure53.de>
2016-08-18 16:55:32 +0200

    Prepared 0.8.3
    Signed-off-by: Mario Heiderich <mario@cure53.de>

--- src/purify.js
+++ src/purify.js
@@ -21,7 +21,7 @@
      * Version label, exposed for easier checks
      * if DOMPurify is up to date or not
      */
-    DOMPurify.version = '0.8.2';
+    DOMPurify.version = '0.8.3';
 
     /**
      * Array of elements that DOMPurify removed during sanitation.
