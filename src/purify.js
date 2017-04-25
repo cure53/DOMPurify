da2484d19b61b5eb35a8dad3460f0b193ac8cb2c
Mario Heiderich <mario@cure53.de>
2017-04-25 09:45:59 +0200

    Preparing 0.8.7 release

--- src/purify.js
+++ src/purify.js
@@ -21,7 +21,7 @@
      * Version label, exposed for easier checks
      * if DOMPurify is up to date or not
      */
-    DOMPurify.version = '0.8.6';
+    DOMPurify.version = '0.8.7';
 
     /**
      * Array of elements that DOMPurify removed during sanitation.
