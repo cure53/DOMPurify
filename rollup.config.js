711528220550fd30b18c112cd7dfb6303381eec8
tdeekens <nerd@tdeekens.name>
2017-08-16 08:58:57 +0200

    chore: rollup config to produce targets

--- rollup.config.js
+++ rollup.config.js
@@ -2,37 +2,38 @@ const nodeResolve = require('rollup-plugin-node-resolve');
 const babel = require('rollup-plugin-babel');
 const replace = require('rollup-plugin-replace');
 const uglify = require('rollup-plugin-uglify');
+const bundleSize = require('rollup-plugin-bundle-size');
 
-const env = process.env.NODE_ENV
-const version = process.env.npm_package_version
+const env = process.env.NODE_ENV;
+const version = process.env.npm_package_version;
 
 const config = {
   entry: 'src/purify.js',
   external: [],
   globals: {},
-  format: 'umd',
   moduleName: 'DOMPurify',
   sourceMap: true,
   plugins: [
     nodeResolve(),
     babel({
-      exclude: '**/node_modules/**'
+      exclude: '**/node_modules/**',
     }),
     replace({
       'process.env.NODE_ENV': JSON.stringify(env),
-      'VERSION': `'${version}'`,
-    })
-  ]
-}
+      VERSION: `'${version}'`,
+    }),
+    bundleSize(),
+  ],
+};
 
 if (env === 'production') {
   config.plugins.push(
     uglify({
       compress: {
-        warnings: false
-      }
+        warnings: false,
+      },
     })
-  )
+  );
 }
 
 module.exports = config;
