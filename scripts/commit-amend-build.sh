0ccf8779373fdabd91405e24fdd07200ee9415c2
tdeekens <nerd@tdeekens.name>
2017-08-20 23:52:38 +0200

    build: fix rollup bundling

--- scripts/commit-amend-build.sh
+++ scripts/commit-amend-build.sh
@@ -1,3 +1,3 @@
 echo "# Amending minified assets to HEAD"
 
-git add ./dist/purify.min.js ./dist/purify.min.js.map ./dist/purify.cjs.js ./dist/purify.cjs.js.map ./dist/purify.es.js ./dist/purify.es.js.map
+git add ./dist/purify.js ./dist/purify.js.map ./dist/purify.min.js ./dist/purify.min.js.map ./dist/purify.cjs.js ./dist/purify.cjs.js.map ./dist/purify.es.js ./dist/purify.es.js.map
