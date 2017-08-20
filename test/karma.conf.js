a762940980e8ab7d297bc8456f5e334aaed7adb0
tdeekens <nerd@tdeekens.name>
2017-08-20 20:01:39 +0200

    build: add options to karma run

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,6 +1,19 @@
+const argv = require('minimist')(process.argv.slice(2));
+const isArray = require('lodash.isarray');
 const commonjs = require('rollup-plugin-commonjs');
 const includePaths = require('rollup-plugin-includepaths');
 const rollupConfig = require('../rollup.config.js');
+const customLaunchers = require('./karma.custom-launchers.config.js')
+  .customLaunchers;
+const browsers = require('./karma.custom-launchers.config.js').browsers;
+const browser = require('./karma.custom-launchers.config.js').browser();
+
+const shouldProbeOnly = argv.shouldProbeOnly !== '';
+const shouldTestOnBrowserStack = argv.shouldTestOnBrowserStack !== '';
+const browserStackBrowsers = shouldProbeOnly ? [browser] : browsers;
+const karmaBrowsers = shouldTestOnBrowserStack
+  ? browserStackBrowsers
+  : isArray(argv.browsers) ? argv.browsers.split(' ') : ['Chrome'];
 
 rollupConfig.plugins.push(
   commonjs(),
@@ -42,162 +55,8 @@ module.exports = function(config) {
 
     rollupPreprocessor: rollupConfig,
 
-    customLaunchers: {
-      bs_win81_ie_11: {
-        base: 'BrowserStack',
-        browser: 'ie',
-        browser_version: '11.0',
-        os: 'Windows',
-        os_version: '8.1',
-      },
-      bs_win8_ie_10: {
-        base: 'BrowserStack',
-        browser: 'ie',
-        browser_version: '10.0',
-        os: 'Windows',
-        os_version: '8',
-      },
-      bs_mavericks_chrome_44: {
-        base: 'BrowserStack',
-        device: null,
-        os: 'OS X',
-        browser_version: '44.0',
-        browser: 'chrome',
-        os_version: 'Mavericks',
-      },
-      bs_yosemite_firefox_40: {
-        base: 'BrowserStack',
-        device: null,
-        os: 'OS X',
-        browser_version: '40.0',
-        browser: 'firefox',
-        os_version: 'Yosemite',
-      },
-      bs_sierra_safari_10: {
-        base: 'BrowserStack',
-        device: null,
-        os: 'OS X',
-        browser_version: '10.0',
-        browser: 'safari',
-        os_version: 'Sierra',
-      },
-      bs_yosemite_safari_8: {
-        base: 'BrowserStack',
-        device: null,
-        os: 'OS X',
-        browser_version: '8.0',
-        browser: 'safari',
-        os_version: 'Yosemite',
-      },
-      bs_win81_opera_31: {
-        base: 'BrowserStack',
-        device: null,
-        os: 'Windows',
-        browser_version: '31',
-        browser: 'opera',
-        os_version: '8.1',
-      },
-      bs_win7_firefox_20: {
-        base: 'BrowserStack',
-        device: null,
-        os: 'Windows',
-        browser_version: '20.0',
-        browser: 'firefox',
-        os_version: '7',
-      },
-      bs_win7_firefox_15: {
-        base: 'BrowserStack',
-        device: null,
-        os: 'Windows',
-        browser_version: '15.0',
-        browser: 'firefox',
-        os_version: '7',
-      },
-      bs_win81_chrome_22: {
-        base: 'BrowserStack',
-        device: null,
-        os: 'Windows',
-        browser_version: '22.0',
-        browser: 'chrome',
-        os_version: '8.1',
-      },
-      bs_win10_edge_13: {
-        base: 'BrowserStack',
-        device: null,
-        os: 'Windows',
-        browser_version: '13.0',
-        browser: 'edge',
-        os_version: '10',
-      },
-      bs_win10_edge_14: {
-        base: 'BrowserStack',
-        device: null,
-        os: 'Windows',
-        browser_version: '14.0',
-        browser: 'edge',
-        os_version: '10',
-      },
-      bs_win10_firefox_46: {
-        base: 'BrowserStack',
-        device: null,
-        os: 'Windows',
-        browser_version: '46.0',
-        browser: 'firefox',
-        os_version: '10',
-      },
-      bs_win10_firefox_52: {
-        base: 'BrowserStack',
-        device: null,
-        os: 'Windows',
-        browser_version: '52.0',
-        browser: 'firefox',
-        os_version: '10',
-      },
-      bs_win10_chrome_50: {
-        base: 'BrowserStack',
-        device: null,
-        os: 'Windows',
-        browser_version: '50.0',
-        browser: 'chrome',
-        os_version: '10',
-      },
-      bs_win10_chrome_57: {
-        base: 'BrowserStack',
-        device: null,
-        os: 'Windows',
-        browser_version: '57.0',
-        browser: 'chrome',
-        os_version: '10',
-      },
-      bs_win10_chrome_59: {
-        base: 'BrowserStack',
-        device: null,
-        os: 'Windows',
-        browser_version: '59.0',
-        browser: 'chrome',
-        os_version: '10',
-      },
-    },
-
-    browsers: [
-      'bs_win81_ie_11',
-      'bs_win8_ie_10',
-      'bs_mavericks_chrome_44',
-      'bs_yosemite_firefox_40',
-      'bs_yosemite_safari_8',
-      'bs_sierra_safari_10',
-      'bs_win81_opera_31',
-      'bs_win7_firefox_20',
-      'bs_win7_firefox_15',
-      'bs_win81_chrome_22',
-      'bs_win10_edge_13',
-      'bs_win10_edge_14',
-      'bs_win10_firefox_46',
-      'bs_win10_firefox_52',
-      'bs_win10_chrome_50',
-      'bs_win10_chrome_57',
-      'bs_win10_chrome_59',
-    ],
+    customLaunchers,
+    browsers: karmaBrowsers,
 
     browserDisconnectTimeout: 10000,
     browserDisconnectTolerance: 1,
