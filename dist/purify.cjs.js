19f8c6fce0b4cbb3f16a4089ef77f69be3c8645f
Mario Heiderich <mario@cure53.de>
2017-08-21 09:00:15 +0200

    Prepared 1.0.1 release

--- dist/purify.cjs.js
+++ dist/purify.cjs.js
@@ -63,7 +63,7 @@ function createDOMPurify() {
       * Version label, exposed for easier checks
       * if DOMPurify is up to date or not
       */
-  DOMPurify.version = '1.0.0';
+  DOMPurify.version = '1.0.1';
 
   /**
     * Array of elements that DOMPurify removed during sanitation.
