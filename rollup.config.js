5910bb651cc35b5ca96c29be3f633922ac7818b5
tdeekens <tobias.deekens@commercetools.de>
2017-08-21 13:40:33 +0200

    chore: remove dist/src and add more to files

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,13 @@ const nodeResolve = require('rollup-plugin-node-resolve');
 const babel = require('rollup-plugin-babel');
 const replace = require('rollup-plugin-replace');
 const uglify = require('rollup-plugin-uglify');
-const pkg = require('./package.json');
 
 const env = process.env.NODE_ENV;
 const isProd = env === 'production';
 const version = process.env.npm_package_version;
 
 const config = {
-  entry: pkg.src,
+  entry: 'src/purify.js',
   external: [],
   globals: {},
   format: 'umd',
