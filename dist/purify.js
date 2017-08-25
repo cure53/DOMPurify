8328e11385aab3e7a4260a75a25aa4943a6bbd36
Mario Heiderich <mario@cure53.de>
2017-08-25 16:18:31 +0200

    Preparing 1.0.2 release

--- dist/purify.js
+++ dist/purify.js
@@ -66,7 +66,7 @@ function createDOMPurify() {
       * Version label, exposed for easier checks
       * if DOMPurify is up to date or not
       */
-  DOMPurify.version = '1.0.1';
+  DOMPurify.version = '1.0.2';
 
   /**
     * Array of elements that DOMPurify removed during sanitation.
