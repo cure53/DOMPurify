3356ef45bc1d9bb52a9eccd7fde2ef067363e1cb
Mario Heiderich <mario@cure53.de>
2017-12-08 12:02:31 +0100

    Fixed annyoing npm autoversion issue

--- dist/purify.js
+++ dist/purify.js
@@ -74,7 +74,7 @@ function createDOMPurify() {
    * Version label, exposed for easier checks
    * if DOMPurify is up to date or not
    */
-  DOMPurify.version = '1.1.1';
+  DOMPurify.version = '1.0.3';
 
   /**
    * Array of elements that DOMPurify removed during sanitation.
