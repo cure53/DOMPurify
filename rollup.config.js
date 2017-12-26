24a6b75bc9c784716400a6e0eb440c503306fe3e
tdeekens <nerd@tdeekens.name>
2017-12-26 17:22:14 +0100

    chore: update rollup API

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,39 @@
-const nodeResolve = require('rollup-plugin-node-resolve');
-const babel = require('rollup-plugin-babel');
-const replace = require('rollup-plugin-replace');
-const uglify = require('rollup-plugin-uglify');
+const nodeResolve = require("rollup-plugin-node-resolve");
+const babel = require("rollup-plugin-babel");
+const replace = require("rollup-plugin-replace");
+const uglify = require("rollup-plugin-uglify");
 
 const env = process.env.NODE_ENV;
-const isProd = env === 'production';
+const isProd = env === "production";
 const version = process.env.npm_package_version;
 
 const config = {
-  entry: 'src/purify.js',
+  entry: "src/purify.js",
   external: [],
-  globals: {},
-  format: 'umd',
-  moduleName: 'DOMPurify',
-  sourceMap: true,
+  output: {
+    name: "DOMPurify",
+    globals: {},
+    format: "umd",
+    sourcemap: true
+  },
   plugins: [
     nodeResolve(),
     babel({
-      exclude: '**/node_modules/**',
+      exclude: "**/node_modules/**"
     }),
     replace({
-      'process.env.NODE_ENV': JSON.stringify(env),
-      VERSION: `'${version}'`,
-    }),
-  ],
+      "process.env.NODE_ENV": JSON.stringify(env),
+      VERSION: `'${version}'`
+    })
+  ]
 };
 
 if (isProd) {
   config.plugins.push(
     uglify({
       compress: {
-        warnings: false,
-      },
+        warnings: false
+      }
     })
   );
 }
