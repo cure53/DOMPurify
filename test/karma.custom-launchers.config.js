33ee4251af9406ff5d853d1f5ac898ed737b92a7
Mario Heiderich <mario@cure53.de>
2019-02-19 16:17:57 +0100

    Attempting to fix broken CI (Safari 11.1.2 doesn't respond)

--- test/karma.custom-launchers.config.js
+++ test/karma.custom-launchers.config.js
@@ -39,7 +39,7 @@ const customLaunchers = {
     base: 'BrowserStack',
     device: null,
     os: 'OS X',
-    browser_version: '11.1',
+    browser_version: '11.0',
     browser: 'safari',
     os_version: 'High Sierra',
   },
