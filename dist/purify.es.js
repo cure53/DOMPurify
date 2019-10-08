64fe2f0a94a7b639eb92f247f712362f53593569
Mario Heiderich <mario@cure53.de>
2019-10-08 10:34:42 +0200

    Finalizing 2.0.5 release

--- dist/purify.es.js
+++ dist/purify.es.js
@@ -165,7 +165,7 @@ function createDOMPurify() {
    * Version label, exposed for easier checks
    * if DOMPurify is up to date or not
    */
-  DOMPurify.version = '2.0.4';
+  DOMPurify.version = '2.0.5';
 
   /**
    * Array of elements that DOMPurify removed during sanitation.
