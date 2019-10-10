7acc4c3ed529c31b9632cb5784b3ddd50c32b4f4
Mario Heiderich <mario@cure53.de>
2019-10-10 10:40:35 +0200

    Preparing 2.0.6 release

--- dist/purify.cjs.js
+++ dist/purify.cjs.js
@@ -167,7 +167,7 @@ function createDOMPurify() {
    * Version label, exposed for easier checks
    * if DOMPurify is up to date or not
    */
-  DOMPurify.version = '2.0.5';
+  DOMPurify.version = '2.0.6';
 
   /**
    * Array of elements that DOMPurify removed during sanitation.
