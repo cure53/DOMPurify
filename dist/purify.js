504157a1991c9ab9d6ab54fe45cb6b7583293f91
Mario Heiderich <mario@cure53.de>
2020-03-03 08:18:33 +0100

    Added shorter license header

--- dist/purify.js
+++ dist/purify.js
@@ -1,38 +1,4 @@
-/*
-* DOMPurify
-* Copyright 2015 Mario Heiderich
-* 
-* DOMPurify is free software; you can redistribute it and/or modify it under the
-* terms of either:
-* 
-* a) the Apache License Version 2.0, or
-* b) the Mozilla Public License Version 2.0
-*  
-* -----------------------------------------------------------------------------
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License. 
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-* 
-* -----------------------------------------------------------------------------
-*
-* This Source Code Form is subject to the
-* terms of the Mozilla Public License, v.
-* 2.0. If a copy of the MPL was not
-* distributed with this file, You can
-* obtain one at
-* http://mozilla.org/MPL/2.0/.
-*
-* @license
-*/
+/*! DOMPurify | (c) Cure53 and other contributors | github.com/cure53/DOMPurify/blob/master/LICENSE */
 
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
