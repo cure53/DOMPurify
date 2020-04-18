d0011ab1063b3eaaa96a9b607e633220715fef95
tdeekens <tobias.deekens@commercetools.de>
2020-04-18 22:51:35 +0200

    chore: format rollup config with updated prettier

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,33 @@
-const nodeResolve = require("rollup-plugin-node-resolve");
-const babel = require("rollup-plugin-babel");
-const replace = require("rollup-plugin-replace");
+const nodeResolve = require('rollup-plugin-node-resolve');
+const babel = require('rollup-plugin-babel');
+const replace = require('rollup-plugin-replace');
 const { terser } = require('rollup-plugin-terser');
-const fs = require("fs");
+const fs = require('fs');
 
 const env = process.env.NODE_ENV;
-const isProd = env === "production";
+const isProd = env === 'production';
 const version = process.env.npm_package_version;
 
 const config = {
-  input: "src/purify.js",
+  input: 'src/purify.js',
   external: [],
   output: {
-    name: "DOMPurify",
+    name: 'DOMPurify',
     globals: {},
-    format: "umd",
+    format: 'umd',
     sourcemap: true,
-    banner: fs.readFileSync("./src/license_header")
+    banner: fs.readFileSync('./src/license_header'),
   },
   plugins: [
     nodeResolve(),
     babel({
-      exclude: "**/node_modules/**"
+      exclude: '**/node_modules/**',
     }),
     replace({
-      "process.env.NODE_ENV": JSON.stringify(env),
-      VERSION: `'${version}'`
-    })
-  ]
+      'process.env.NODE_ENV': JSON.stringify(env),
+      VERSION: `'${version}'`,
+    }),
+  ],
 };
 
 if (isProd) {
