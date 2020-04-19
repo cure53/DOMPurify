3afefe71b99b1b601f43f040f1f52da4e15eec00
tdeekens <tobias.deekens@commercetools.de>
2020-04-19 16:59:45 +0200

    fix: having dependency run

--- rollup.config.js
+++ rollup.config.js
@@ -21,7 +21,7 @@ const config = {
   plugins: [
     nodeResolve(),
     babel({
-      exclude: '**/node_modules/**',
+      exclude: ['**/node_modules/**'],
     }),
     replace({
       'process.env.NODE_ENV': JSON.stringify(env),
