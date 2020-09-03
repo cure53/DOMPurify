c025bc8070a5a8fc2c5550d177a4130d45691b94
Mario Heiderich <mario@cure53.de>
2020-09-03 12:06:12 +0200

    chore: preparing 2.0.15 release

--- dist/purify.js
+++ dist/purify.js
@@ -214,7 +214,7 @@
      * Version label, exposed for easier checks
      * if DOMPurify is up to date or not
      */
-    DOMPurify.version = '2.0.14';
+    DOMPurify.version = '2.0.15';
 
     /**
      * Array of elements that DOMPurify removed during sanitation.
