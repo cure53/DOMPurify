63061bf434836904e73404682ad6fe1e64f8e09d
Mario Heiderich <mario@cure53.de>
2020-09-18 14:23:32 +0200

    chore: Preparing 2.0.16 release

--- dist/purify.es.js
+++ dist/purify.es.js
@@ -208,7 +208,7 @@ function createDOMPurify() {
    * Version label, exposed for easier checks
    * if DOMPurify is up to date or not
    */
-  DOMPurify.version = '2.0.15';
+  DOMPurify.version = '2.0.16';
 
   /**
    * Array of elements that DOMPurify removed during sanitation.
