e43de71cfd76571f825913bd1042e4736465782e
Mario Heiderich <mario@cure53.de>
2020-11-02 20:55:31 +0100

    fix: squished another variation of the mXSS

--- src/purify.js
+++ src/purify.js
@@ -651,7 +651,7 @@ function createDOMPurify(window = getGlobal()) {
     /* Take care of an mXSS pattern using p, br inside svg, math */
     if (
       (tagName === 'svg' || tagName === 'math') &&
-      currentNode.querySelectorAll('p, br, form').length !== 0
+      currentNode.querySelectorAll('p, br, form, table').length !== 0
     ) {
       _forceRemove(currentNode);
       return true;
