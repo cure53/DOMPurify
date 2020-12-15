499b3bbe44e316449b6a05afb655391a9d55cee9
Mario Heiderich <mario@cure53.de>
2020-12-15 17:33:10 +0100

    chore: Prepare 2.2.4 release

--- dist/purify.cjs.js
+++ dist/purify.cjs.js
@@ -205,7 +205,7 @@ function createDOMPurify() {
    * Version label, exposed for easier checks
    * if DOMPurify is up to date or not
    */
-  DOMPurify.version = '2.2.3';
+  DOMPurify.version = '2.2.4';
 
   /**
    * Array of elements that DOMPurify removed during sanitation.
