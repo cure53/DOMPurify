b11cb72fd45b9489ad989517e22b6141743a9fa3
Mario Heiderich <mario@cure53.de>
2020-12-18 16:12:06 +0100

    chore: Preparing 2.2.6 release after failed 2.2.5 attempt /2

--- dist/purify.es.js
+++ dist/purify.es.js
@@ -238,7 +238,7 @@ function createDOMPurify() {
    * Version label, exposed for easier checks
    * if DOMPurify is up to date or not
    */
-  DOMPurify.version = '2.2.5';
+  DOMPurify.version = '2.2.6';
 
   /**
    * Array of elements that DOMPurify removed during sanitation.
