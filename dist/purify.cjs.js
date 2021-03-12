a9ad5be0fc28e523a07cfca72e58829cecfd491a
Mario Heiderich <mario@cure53.de>
2021-03-12 16:19:17 +0100

    chore: added correct version tags for 2.2.7

--- dist/purify.cjs.js
+++ dist/purify.cjs.js
@@ -245,7 +245,7 @@ function createDOMPurify() {
    * Version label, exposed for easier checks
    * if DOMPurify is up to date or not
    */
-  DOMPurify.version = '2.2.6';
+  DOMPurify.version = '2.2.7';
 
   /**
    * Array of elements that DOMPurify removed during sanitation.
--- dist/purify.es.js
+++ dist/purify.es.js
@@ -243,7 +243,7 @@ function createDOMPurify() {
    * Version label, exposed for easier checks
    * if DOMPurify is up to date or not
    */
-  DOMPurify.version = '2.2.6';
+  DOMPurify.version = '2.2.7';
 
   /**
    * Array of elements that DOMPurify removed during sanitation.
