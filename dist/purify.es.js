9de5b19e5d701db408318df0f2935c74b25b7b09
Mario Heiderich <mario@cure53.de>
2021-06-01 12:31:24 +0200

    chore: Preparing 2.2.9 release

--- dist/purify.es.js
+++ dist/purify.es.js
@@ -243,7 +243,7 @@ function createDOMPurify() {
    * Version label, exposed for easier checks
    * if DOMPurify is up to date or not
    */
-  DOMPurify.version = '2.2.8';
+  DOMPurify.version = '2.2.9';
 
   /**
    * Array of elements that DOMPurify removed during sanitation.
--- dist/purify.js
+++ dist/purify.js
@@ -249,7 +249,7 @@
      * Version label, exposed for easier checks
      * if DOMPurify is up to date or not
      */
-    DOMPurify.version = '2.2.8';
+    DOMPurify.version = '2.2.9';
 
     /**
      * Array of elements that DOMPurify removed during sanitation.
