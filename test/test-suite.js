aca1fee0d7b7d79498cce3e28265bafbe803a6e6
Mario Heiderich <mario@cure53.de>
2021-09-14 10:00:11 +0200

    test: Fixed tests for MSIE & Edge 18

--- test/test-suite.js
+++ test/test-suite.js
@@ -1610,6 +1610,7 @@ module.exports = function (DOMPurify, window, tests, xssTests) {
                 '<a href="#">invalid</a><a title="title" href="#">valid</a>'
            ],
           'application/xhtml+xml': [
+                '<a href="#">invalid</a><a href="#" title="title">valid</a>',
                 'invalid<a xmlns="http://www.w3.org/1999/xhtml" href="#">valid</a>',
                 '<a href="#">invalid</a><a title="title" href="#">valid</a>'
            ],
@@ -1635,7 +1636,8 @@ module.exports = function (DOMPurify, window, tests, xssTests) {
           ],
           'application/xhtml+xml': [
                 '<html xmlns="http://www.w3.org/1999/xhtml"><head></head><body>invalid<a href="#">valid</a></body></html>',
-                '<html><head></head><body><a href="#">invalid</a><a title="title" href="#">valid</a></body></html>'
+                '<html><head></head><body><a href="#">invalid</a><a title="title" href="#">valid</a></body></html>',
+                '<html><head></head><body><a href="#">invalid</a><a href="#" title="title">valid</a></body></html>'
           ]
         }
       }
