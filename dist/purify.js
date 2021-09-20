7fbe07c632906a7ca3b9534fc00e7c12ce2e25e9
Mario Heiderich <mario@cure53.de>
2021-09-20 14:27:26 +0200

    chore: Preparing 2.3.3 release

--- dist/purify.js
+++ dist/purify.js
@@ -1,4 +1,4 @@
-/*! @license DOMPurify 2.3.2 | (c) Cure53 and other contributors | Released under the Apache license 2.0 and Mozilla Public License 2.0 | github.com/cure53/DOMPurify/blob/2.3.2/LICENSE */
+/*! @license DOMPurify 2.3.3 | (c) Cure53 and other contributors | Released under the Apache license 2.0 and Mozilla Public License 2.0 | github.com/cure53/DOMPurify/blob/2.3.3/LICENSE */
 
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
@@ -249,7 +249,7 @@
      * Version label, exposed for easier checks
      * if DOMPurify is up to date or not
      */
-    DOMPurify.version = '2.3.2';
+    DOMPurify.version = '2.3.3';
 
     /**
      * Array of elements that DOMPurify removed during sanitation.
