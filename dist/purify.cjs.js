c84f6e4bb87b05f68ccf0ab471c97b80955779e9
Mario Heiderich <mario@cure53.de>
2022-02-16 08:16:18 +0100

    chore: preparing 2.3.6 release

--- dist/purify.cjs.js
+++ dist/purify.cjs.js
@@ -1,4 +1,4 @@
-/*! @license DOMPurify 2.3.5 | (c) Cure53 and other contributors | Released under the Apache license 2.0 and Mozilla Public License 2.0 | github.com/cure53/DOMPurify/blob/2.3.5/LICENSE */
+/*! @license DOMPurify 2.3.6 | (c) Cure53 and other contributors | Released under the Apache license 2.0 and Mozilla Public License 2.0 | github.com/cure53/DOMPurify/blob/2.3.6/LICENSE */
 
 'use strict';
 
@@ -246,7 +246,7 @@ function createDOMPurify() {
    * Version label, exposed for easier checks
    * if DOMPurify is up to date or not
    */
-  DOMPurify.version = '2.3.5';
+  DOMPurify.version = '2.3.6';
 
   /**
    * Array of elements that DOMPurify removed during sanitation.
