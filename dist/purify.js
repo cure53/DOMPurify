caaae5ed3206082299518351da1c3fd1a66532f0
Mario Heiderich <mario@cure53.de>
2022-08-23 17:36:31 +0200

    chore: prepare 2.3.12 release

--- dist/purify.js
+++ dist/purify.js
@@ -1,4 +1,4 @@
-/*! @license DOMPurify 2.3.11 | (c) Cure53 and other contributors | Released under the Apache license 2.0 and Mozilla Public License 2.0 | github.com/cure53/DOMPurify/blob/2.3.11/LICENSE */
+/*! @license DOMPurify 2.3.12 | (c) Cure53 and other contributors | Released under the Apache license 2.0 and Mozilla Public License 2.0 | github.com/cure53/DOMPurify/blob/2.3.12/LICENSE */
 
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
@@ -326,7 +326,7 @@
      */
 
 
-    DOMPurify.version = '2.3.11';
+    DOMPurify.version = '2.3.12';
     /**
      * Array of elements that DOMPurify removed during sanitation.
      * Empty if nothing was removed.
