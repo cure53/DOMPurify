fade5062ff936cb7faa7e1c2c996d7f782808f5b
Mario Heiderich <mario@cure53.de>
2023-01-06 15:10:04 +0100

    chore: Prepare 2.4.3, final feature release compatible w. MSIE10/11

--- dist/purify.js
+++ dist/purify.js
@@ -1,4 +1,4 @@
-/*! @license DOMPurify 2.4.2 | (c) Cure53 and other contributors | Released under the Apache license 2.0 and Mozilla Public License 2.0 | github.com/cure53/DOMPurify/blob/2.4.2/LICENSE */
+/*! @license DOMPurify 2.4.3 | (c) Cure53 and other contributors | Released under the Apache license 2.0 and Mozilla Public License 2.0 | github.com/cure53/DOMPurify/blob/2.4.3/LICENSE */
 
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
@@ -328,7 +328,7 @@
      */
 
 
-    DOMPurify.version = '2.4.2';
+    DOMPurify.version = '2.4.3';
     /**
      * Array of elements that DOMPurify removed during sanitation.
      * Empty if nothing was removed.
