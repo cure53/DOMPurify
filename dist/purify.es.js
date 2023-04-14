5532856a08c9c6f86237c5e19947d86041fa687d
Dr.-Ing. Mario Heiderich <mario@cure53.de>
2023-04-14 18:40:48 +0200

    chore: preparing 3.0.2 release

--- dist/purify.es.js
+++ dist/purify.es.js
@@ -1,4 +1,4 @@
-/*! @license DOMPurify 3.0.1 | (c) Cure53 and other contributors | Released under the Apache license 2.0 and Mozilla Public License 2.0 | github.com/cure53/DOMPurify/blob/3.0.1/LICENSE */
+/*! @license DOMPurify 3.0.2 | (c) Cure53 and other contributors | Released under the Apache license 2.0 and Mozilla Public License 2.0 | github.com/cure53/DOMPurify/blob/3.0.2/LICENSE */
 
 const {
   entries,
@@ -251,7 +251,7 @@ function createDOMPurify() {
    */
 
 
-  DOMPurify.version = '3.0.1';
+  DOMPurify.version = '3.0.2';
   /**
    * Array of elements that DOMPurify removed during sanitation.
    * Empty if nothing was removed.
