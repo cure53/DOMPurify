68ad9a96a5fd0bdf9a2fc147b4cfc2bb4ed0664d
ssi02014 <ssi02014@naver.com>
2023-08-09 19:34:08 +0900

    fix(purify): Remove unnecessary conditional expressions

--- dist/purify.js
+++ dist/purify.js
@@ -1060,7 +1060,7 @@
       /* Detect mXSS attempts abusing namespace confusion */
 
 
-      if (currentNode.hasChildNodes() && !_isNode(currentNode.firstElementChild) && (!_isNode(currentNode.content) || !_isNode(currentNode.content.firstElementChild)) && regExpTest(/<[/\w]/g, currentNode.innerHTML) && regExpTest(/<[/\w]/g, currentNode.textContent)) {
+      if (currentNode.hasChildNodes() && !_isNode(currentNode.firstElementChild) && regExpTest(/<[/\w]/g, currentNode.innerHTML) && regExpTest(/<[/\w]/g, currentNode.textContent)) {
         _forceRemove(currentNode);
 
         return true;
