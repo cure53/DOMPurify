0cf9d2d7af39d1656e6fb278445e7b1ef6f571b1
Dr.-Ing. Mario Heiderich <mario@cure53.de>
2024-04-06 18:06:26 +0200

    chore: Preparing 3.1.0 release

--- dist/purify.js
+++ dist/purify.js
@@ -1,4 +1,4 @@
-/*! @license DOMPurify 3.0.11 | (c) Cure53 and other contributors | Released under the Apache license 2.0 and Mozilla Public License 2.0 | github.com/cure53/DOMPurify/blob/3.0.11/LICENSE */
+/*! @license DOMPurify 3.1.0 | (c) Cure53 and other contributors | Released under the Apache license 2.0 and Mozilla Public License 2.0 | github.com/cure53/DOMPurify/blob/3.1.0/LICENSE */
 
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
@@ -288,7 +288,7 @@
      * Version label, exposed for easier checks
      * if DOMPurify is up to date or not
      */
-    DOMPurify.version = '3.0.11';
+    DOMPurify.version = '3.1.0';
 
     /**
      * Array of elements that DOMPurify removed during sanitation.
