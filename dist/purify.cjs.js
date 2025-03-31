e62e3efa68c46dbc34b6e63545c6ae8ed20b49f8
hhk-png <fangluoabc@163.com>
2025-03-31 21:26:48 +0800

    fix: Using ALLOWED_URI_REGEXP with the 'g' flag leads to incorrect results

--- dist/purify.cjs.js
+++ dist/purify.cjs.js
@@ -60,6 +60,9 @@ const typeErrorCreate = unconstruct(TypeError);
  */
 function unapply(func) {
   return function (thisArg) {
+    if (thisArg instanceof RegExp) {
+      thisArg.lastIndex = 0;
+    }
     for (var _len = arguments.length, args = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
       args[_key - 1] = arguments[_key];
     }
